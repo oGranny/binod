868f7a3f4d685c627d892f278f3f5cdc28a8fdc0
oGranny <shash199655@gmail.com>
2020-08-11 21:29:52 +0530

    Initial commit

--- binod.py
+++ binod.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
+
 # -*- coding: utf-8 -*-
 #
-#  untitled.py
+#  binod.py
 #  
-#  Copyright 2020 Granny
+#  Copyright 2020 oGranny
 #  
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -27,11 +28,10 @@ from turtle import *
 import time
 
 i = 0
-screensize(5000,5000)
+screensize(2000,2000)
 while True:
-	print(position())
 	home()
-	speed(6)
+	speed(1)
 	right(90)
 	forward(200)
 	left(90)
@@ -92,12 +92,78 @@ while True:
 	#~~~~~~~~~~~N~~~~~~~~~~~~#
 	
 	right(90)
+	forward(150)
+	pendown()
+	right(90)
 	forward(200)
+	left(90)
+	forward(20)
+	left(90)
+	forward(150)
+	right(160)
+	forward(160)
+	left(70)
+	forward(20)
+	left(90)
+	forward(200)
+	left(90)
+	forward(20)
+	left(90)
+	forward(150)
+	right(160)
+	forward(160)
+	left(70)
+	forward(20)
+	penup()
+	############N#############
 	
-			
-			
+	#~~~~~~~~~~~O~~~~~~~~~~~~#
+
+	right(180)
+	forward(135)
+	right(90)
+	forward(100)
+	pendown()
+	circle(100, 360)
+	penup()
+	forward(80)
+	left(90)
+	forward(100)
+	pendown()
+	circle(80, 360)
+	penup()
+	############O##############
+	
+	#~~~~~~~~~~~D~~~~~~~~~~~~~#
+	right(90)
+	forward(20)
+	left(90)
+	forward(150)
+	pendown()
+	right(180)
+	forward(20)
+	right(90)
+	forward(200)
+	right(90)
+	forward(20)
+	penup()
+	right(90)
+	forward(200)
+	pendown()
+	left(90)
+	circle(100, 180)
+	left(90)
+	penup()
+	forward(20)
+	pendown()
+	forward(160)
+	left(90)
+	circle(80, 180)
+	#############D############
+
+
+
 	i += 1
 	print(f"looped {i} times")
 	penup()
-	time.sleep(20000000)
 	# ~ break
